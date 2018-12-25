5e691f5c36332f569bb4aaad0f3b71dce227d287
Lin Luo <l.luo-pi@outlook.com>
2018-12-24 16:30:37 -0800

    Initial Commit

---
+++ LeetCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by Lin Luo on 12/24/18.
+//  Copyright © 2018 Linfinity. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
